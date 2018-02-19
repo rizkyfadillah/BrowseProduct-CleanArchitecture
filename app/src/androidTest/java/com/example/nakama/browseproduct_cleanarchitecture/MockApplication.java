52bcf95c2d7cdc87173f3e3f18991f75193610dc
Rizky Fadillah <rizkyfadillah1994@gmail.com>
2018-02-19 14:14:01 +0700

    remove unused code

--- app/src/androidTest/java/com/example/nakama/browseproduct_cleanarchitecture/MockApplication.java
+++ app/src/androidTest/java/com/example/nakama/browseproduct_cleanarchitecture/MockApplication.java
@@ -9,24 +9,15 @@ import com.example.nakama.browseproduct_cleanarchitecture.di.MainActivityCompone
 
 public class MockApplication extends BrowseProductApplication {
 
-    private AppComponent component;
     private MainActivityComponent mainActivityComponent;
-
-    public void setTestComponent(AppComponent component) {
-        this.component = component;
-    }
 
     public void setMainActivityComponent(MainActivityComponent mainActivityComponent) {
         this.mainActivityComponent = mainActivityComponent;
     }
 
     @Override
-    public AppComponent getComponent() {
-        return component;
-    }
-
-    @Override
     public MainActivityComponent getMainActivityComponent() {
         return mainActivityComponent;
     }
+
 }
